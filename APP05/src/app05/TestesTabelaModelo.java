40b846415dd84bae315ace6d78ce0c901cb70ac7
diogenesprocode <diogenes_person@outlook.com>
2015-05-18 21:31:48 -0300

    Criação da tela

---
+++ APP05/src/app05/TestesTabelaModelo.java
@@ -0,0 +1,25 @@
+package app05;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * @author DiógenesGalileu
+ */
+public class TestesTabelaModelo extends AbstractTableModel{
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
