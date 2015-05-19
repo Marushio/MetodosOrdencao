bc631b84310844731e14e52d08812c2453a44b0f
diogenesprocode <diogenes_person@outlook.com>
2015-05-18 22:57:06 -0300

    Construção da tabela
    Alterações na classe TestesTabelaModelo

--- APP05/src/app05/TestesTabelaModelo.java
+++ APP05/src/app05/TestesTabelaModelo.java
@@ -5,21 +5,18 @@ import javax.swing.table.AbstractTableModel;
 /**
  * @author DiógenesGalileu
  */
-public class TestesTabelaModelo extends AbstractTableModel{
-
-    @Override
-    public int getRowCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int getColumnCount() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+public abstract class TestesTabelaModelo extends AbstractTableModel{
+    BubbleSort bubble = new BubbleSort();
+    InsertionSort insertion = new InsertionSort();
+    MergeSort merge = new MergeSort();
+    QuickSort quick = new QuickSort();
     
+    String [] nomeColunas = {"Algoritmo", "Tempo de Execução", "Qntd. de Trocas"};
+    
+    Object [][] dados = {
+        {"Bubble Sort", bubble.getTempoExecucao(),bubble.getTrocas()},
+        {"Insertion Sort", insertion.getTempoExecucao(), insertion.getTrocas()},
+        {"Merge Sort", "", "", ""},
+        {"Quick Sort", quick.getTempoExecucao(), quick.getTrocas()}
+    };
 }
